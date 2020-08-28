a4c060bfa7c73fa33ee6139f050ff0188ba12900
William T Cao <williamacao@gmail.com>
2020-08-28 22:14:03 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,11 +1,4 @@
-def hello(array)
-  i = 0
-  while i < array.length
-  binding.pry
-    yield(array[i])
-    i += 1
-  end
-end
+
 
 binding.pry
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
